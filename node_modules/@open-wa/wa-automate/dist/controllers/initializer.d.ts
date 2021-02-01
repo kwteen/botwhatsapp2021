526b37dfb11ec6556c0d026af21b901679e71519
AkaShiro22 <35227670+AkaShiro22@users.noreply.github.com>
2021-02-02 00:59:07 +0700

    Delete node_modules directory

--- node_modules/@open-wa/wa-automate/dist/controllers/initializer.d.ts
+++
@@ -1,3 +0,0 @@
-import { Client } from '../api/Client';
-import { ConfigObject } from '../api/model/index';
-export declare function create(_sessionId?: string | ConfigObject, config?: ConfigObject, customUserAgent?: string): Promise<Client>;
--- node_modules/@open-wa/wa-automate/dist/controllers/launch_checks.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function checkWAPIHash(): Promise<boolean>;
-export declare function integrityCheck(waPage: any, notifier: any, spinner: any, debugInfo: any): Promise<boolean>;
